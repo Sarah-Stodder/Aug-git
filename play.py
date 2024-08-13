adcb24126d529ea9d6a46de8459aa0a142110b85
Sarah-Stodder <sjsarahboo@gmail.com>
2024-08-13 13:47:59 -0600

    fixed bug, added new things

--- play.py
+++ play.py
@@ -1,5 +1,16 @@
+import time
+
+def moviator2():
+    ask = input("What do you need to do? ").lower()
+    return f"lets get {ask} started! You got this"
+
+start_time = time.time()
+
+
+
+
 def moviator():
-    ask = input("What do you need to do? ")
+    ask = input("What do you need to do? ").lower()
     if ask == 'dishes':
         print(f"Get scrub daddy on the phone!")
     elif ask == 'laundry':
@@ -7,3 +18,6 @@ def moviator():
     print(f'YOU NEED TO GET {ask} DONE< WHAT ARE YOU DOING? YOU GOT THIS! JUST LIK SHIA "JUST DO IT" ')
 
     
+end_time = start_time - time.time()
+
+print( start_time, moviator(), end_time )
