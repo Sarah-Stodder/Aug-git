d567aa6fa3614be5b2aaf3156a3941a3d40cb64d
Sarah-Stodder <sjsarahboo@gmail.com>
2024-08-13 13:43:42 -0600

    updating main's motivator code

--- play.py
+++ play.py
@@ -1,5 +1,9 @@
 def moviator():
     ask = input("What do you need to do? ")
+    if ask == 'dishes':
+        print(f"Get scrub daddy on the phone!")
+    elif ask == 'laundry':
+        print(f'lets just throw it all away and go shopping')
     print(f'YOU NEED TO GET {ask} DONE< WHAT ARE YOU DOING? YOU GOT THIS! JUST LIK SHIA "JUST DO IT" ')
 
     
